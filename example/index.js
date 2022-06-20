15e4c54b27726bd0ed9f4bfa2a91bb382bde783d
Matthias Brunel <matthias.brunel@mithrilsecurity.io>
2022-06-20 16:00:52 +0200

    simplify example

--- example/index.js
+++ example/index.js
@@ -1,34 +1,10 @@
-import { Tokenizer } from "tokenizers-js";
+import { Tokenizer } from "huggingface-tokenizers-bindings";
 
-let tokenizer;
-const fileBrowser = document.getElementById("browser");
-fileBrowser.addEventListener("change", handleFile, false);
-const nameField = document.getElementById("name");
-const loadButton = document.getElementById("load");
-loadButton.addEventListener("click", handlename, false);
-const textField = document.getElementById("text");
-const tokenizeButton = document.getElementById("tokenize");
-tokenizeButton.addEventListener("click", handleInput, false);
-const outputIds = document.getElementById("output-ids");
-const outputText = document.getElementById("output-tokens");
-
-function handleFile() {
-  let reader = new FileReader();
-  reader.onload = function(event) {
-    let json = event.target.result;
-    tokenizer = new Tokenizer(json);
-  };
-  reader.readAsText(fileBrowser.files[0]);
+async function main() {
+    let tokenizer = await Tokenizer.from_pretrained("gpt2");
+    let encoding = tokenizer.encode("I love AI and privacy", false);
+    console.log(encoding.input_ids);
+    console.log(encoding.tokens);
 }
 
-async function handlename() {
-  let name = nameField.value;
-  tokenizer = await Tokenizer.from_pretrained(name);
-}
-
-function handleInput() {
-  let text = textField.value;
-  let encoding = tokenizer.encode(text, false);
-  outputIds.innerHTML = "ids : [" + encoding.input_ids + "]";
-  outputText.innerHTML = "tokens : [" + encoding.tokens + "]";
-}
+main();
--- example/setup.sh
+++
@@ -1,3 +0,0 @@
-wasm-pack build ..
-npm --prefix ../tokenizers-js install ../tokenizers-js
-npm install
