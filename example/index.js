a2602d02e9164d3c02367ecdf6459d94cc563404
Matthias Brunel <matthias.brunel@mithrilsecurity.io>
2022-06-30 14:27:52 +0200

    better example

--- example/index.js
+++ example/index.js
@@ -1,10 +1,13 @@
 import { Tokenizer } from "huggingface-tokenizers-bindings";
 
+const INPUT = "I love AI and privacy";
+
 async function main() {
     let tokenizer = await Tokenizer.from_pretrained("gpt2");
-    let encoding = tokenizer.encode("I love AI and privacy", false);
-    console.log(encoding.input_ids);
-    console.log(encoding.tokens);
+    let encoding = tokenizer.encode(INPUT, false);
+    document.getElementById("input").innerHTML = INPUT;
+    document.getElementById("tokens").innerHTML = "[" + encoding.tokens + "]"
+    document.getElementById("input_ids").innerHTML = "[" + encoding.input_ids + "]";
 }
 
 main();
